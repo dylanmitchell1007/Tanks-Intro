a1e3194d28e5ccd291e024a85883f4763c4b6c54
dylanmitchell1007 <armadummy@yahoo.com>
2016-10-21 14:16:33 -0500

    Init Commit

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -27,67 +27,74 @@ public class TankMovement : MonoBehaviour
 
     private void OnEnable()
     {
+        // When the tank is turned on, make sure it's not kinematic.
         m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
         m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
+        m_TurnInputValue = 0;
     }
 
 
     private void OnDisable()
     {
+        // When the tank is turned off, set it to kinematic so it stops moving.
         m_Rigidbody.isKinematic = true;
     }
 
 
     private void Start()
     {
+        // The axes names are based on player number.
         m_MovementAxisName = "Vertical" + m_PlayerNumber;
         m_TurnAxisName = "Horizontal" + m_PlayerNumber;
 
+        // Store the original pitch of the audio source.
         m_OriginalPitch = m_MovementAudio.pitch;
     }
 
 
     private void Update()
     {
-        // Store the player's input and make sure the audio for the engine is playing.
+        // Store the value of both input axes.
         m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
         m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
         EngineAudio();
-        
     }
 
 
     private void EngineAudio()
     {
-        // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
-        if(Mathf.Abs (m_MovementInputValue)<0.1f && Mathf.Abs (m_TurnInputValue)<0.1f)
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 3.1f)
         {
-            if(m_MovementAudio.clip == m_EngineDriving)
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
             {
+                // ... change the clip to idling and play it.
                 m_MovementAudio.clip = m_EngineIdling;
                 m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
                 m_MovementAudio.Play();
             }
         }
-
         else
         {
-            if(m_MovementAudio.clip==m_EngineDriving)
+            // Otherwise if the tank is moving and if the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
             {
+                // ... change the clip to driving and play.
                 m_MovementAudio.clip = m_EngineDriving;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch+m_PitchRange);
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
                 m_MovementAudio.Play();
             }
         }
-
-
     }
 
 
     private void FixedUpdate()
     {
-        // Move and turn the tank.
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
         Move();
         Turn();
     }
@@ -95,24 +102,23 @@ public class TankMovement : MonoBehaviour
 
     private void Move()
     {
-        // Adjust the position of the tank based on the player's input.
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
         Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
+        // Apply this movement to the rigidbody's position.
         m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-
     }
 
 
     private void Turn()
     {
-        // Adjust the rotation of the tank based on the player's input.
-
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
         float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
+        // Make this into a rotation in the y axis.
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
 
+        // Apply this rotation to the rigidbody's rotation.
         m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
-
-
     }
 }
